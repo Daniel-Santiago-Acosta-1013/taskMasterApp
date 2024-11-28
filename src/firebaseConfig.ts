d3ffc83c2ec5b1bc3698c4b852e5432ecad86add
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2024-11-28 13:17:37 -0500

    Refactor firebaseConfig.ts to remove console logs of Firebase environment variables

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -2,14 +2,6 @@ import { initializeApp } from "firebase/app";
 import { initializeFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-console.log(import.meta.env.VITE_FIREBASE_API_KEY);
-console.log(import.meta.env.VITE_FIREBASE_AUTH_DOMAIN);
-console.log(import.meta.env.VITE_FIREBASE_PROJECT_ID);
-console.log(import.meta.env.VITE_FIREBASE_STORAGE_BUCKET);
-console.log(import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID);
-console.log(import.meta.env.VITE_FIREBASE_APP_ID);
-console.log(import.meta.env.VITE_FIREBASE_MEASUREMENT_ID);
-
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
