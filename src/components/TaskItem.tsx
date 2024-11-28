9c74a66d25a327ffa0512338bbd6c5092dd2480e
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2024-11-28 14:32:16 -0500

    Refactor TaskItem component to add editing functionality

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,4 +1,5 @@
 import { useDispatch } from "react-redux";
+import { useState } from "react";
 import { updateTaskAsync, deleteTaskAsync, Task } from "../features/tasks/tasksSlice";
 import { AppDispatch } from "../store";
 
@@ -9,12 +10,29 @@ interface TaskItemProps {
 function TaskItem({ task }: TaskItemProps) {
   const dispatch = useDispatch<AppDispatch>();
 
+  const [isEditing, setIsEditing] = useState(false);
+  const [newTitle, setNewTitle] = useState(task.title);
+
   const handleComplete = () => {
     dispatch(updateTaskAsync({ ...task, completed: !task.completed }));
   };
 
   const handleDelete = () => {
     dispatch(deleteTaskAsync(task.id));
+  };
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+
+  const handleSave = () => {
+    dispatch(updateTaskAsync({ ...task, title: newTitle }));
+    setIsEditing(false);
+  };
+
+  const handleCancel = () => {
+    setNewTitle(task.title);
+    setIsEditing(false);
   };
 
   return (
@@ -25,18 +43,53 @@ function TaskItem({ task }: TaskItemProps) {
         onChange={handleComplete}
         className="mr-4 h-5 w-5 text-blue-500"
       />
-      <span
-        className={`flex-grow text-gray-800 ${task.completed ? "line-through text-gray-400" : ""
+      {isEditing ? (
+        <input
+          type="text"
+          value={newTitle}
+          onChange={(e) => setNewTitle(e.target.value)}
+          className="flex-grow border border-gray-300 rounded-md px-2 py-1"
+        />
+      ) : (
+        <span
+          className={`flex-grow text-gray-800 ${
+            task.completed ? "line-through text-gray-400" : ""
           }`}
-      >
-        {task.title}
-      </span>
-      <button
-        onClick={handleDelete}
-        className="text-red-500 hover:text-red-600 transition duration-200"
-      >
-        Eliminar
-      </button>
+        >
+          {task.title}
+        </span>
+      )}
+      {isEditing ? (
+        <>
+          <button
+            onClick={handleSave}
+            className="text-green-500 hover:text-green-600 transition duration-200 ml-2"
+          >
+            Guardar
+          </button>
+          <button
+            onClick={handleCancel}
+            className="text-gray-500 hover:text-gray-600 transition duration-200 ml-2"
+          >
+            Cancelar
+          </button>
+        </>
+      ) : (
+        <>
+          <button
+            onClick={handleEdit}
+            className="text-blue-500 hover:text-blue-600 transition duration-200 ml-2"
+          >
+            Editar
+          </button>
+          <button
+            onClick={handleDelete}
+            className="text-red-500 hover:text-red-600 transition duration-200 ml-2"
+          >
+            Eliminar
+          </button>
+        </>
+      )}
     </div>
   );
 }
